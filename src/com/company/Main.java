8ae11b5f8fb4ccc11cae3b29fb901bf661626854
Артем Бетанов <artembetanov@MacBook-Pro-Artem-2.local>
2020-11-03 09:21:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exception exception = new Exception();
+        exception.exceptionDemo();
+    }
+}
